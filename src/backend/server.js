2d8f0fd5d08011ea0838d37546462b7c2843d0da
Aman Taak <amantaak99@hotmail.com>
2024-07-26 01:20:39 +0100

    update

--- src/backend/server.js
+++ src/backend/server.js
@@ -473,7 +473,7 @@ async function handlePaymentIntentSucceeded(paymentIntent) {
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
