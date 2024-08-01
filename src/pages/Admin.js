7119950cd6678c2102173a272108df52f0f90552
Aman Taak <amantaak99@hotmail.com>
2024-08-01 18:08:28 +0100

    update

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { format } from "date-fns";
 import "../styles/admin.css";
+import axios from "axios";
 
 const AdminDashboard = () => {
   const [data, setData] = useState([]);
@@ -8,7 +9,9 @@ const AdminDashboard = () => {
   useEffect(() => {
     const fetchAdminData = async () => {
       try {
-        const response = await fetch("https://www.citbcertify.co.uk/api/admin");
+        const response = await axios.get(
+          "https://www.citbcertify.co.uk/api/admin"
+        );
         const result = await response.json();
         setData(result.data);
       } catch (error) {
