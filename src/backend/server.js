14c6efeb3c4fd9b9a5facf9f4f425f81d8e72aba
Aman Taak <amantaak99@hotmail.com>
2024-08-01 01:00:32 +0100

    update

--- src/backend/server.js
+++ src/backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://citbcertify.co.uk",
+    origin: "https://www.citbcertify.co.uk",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
