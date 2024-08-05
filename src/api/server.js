652c1557712ade1559833eb9f6c7d7379d2f57c0
Aman Taak <amantaak99@hotmail.com>
2024-08-06 00:37:48 +0100

    update

--- src/api/server.js
+++ src/api/server.js
@@ -503,10 +503,6 @@ async function handlePaymentIntentSucceeded(paymentIntent) {
   console.log("Payment Intent succeeded:", paymentIntent.id);
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../build/index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
