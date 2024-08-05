d65f734ae9e2c23e88dab911ee5c0297f4c1b2b2
Aman Taak <amantaak99@hotmail.com>
2024-08-05 20:38:02 +0100

    update

--- src/api/server.js
+++ src/api/server.js
@@ -12,10 +12,9 @@ const stripe = require("stripe")(
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 const ENDPOINT_SECRET = process.env.STRIPE_ENDPOINT_SECRET;
 
-app.use(express.static(path.join(__dirname, "../build")));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
@@ -502,10 +501,6 @@ async function handleCheckoutSessionCompleted(session) {
 async function handlePaymentIntentSucceeded(paymentIntent) {
   console.log("Payment Intent succeeded:", paymentIntent.id);
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
