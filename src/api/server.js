9250b21fa150363a7114788d3b002dc8b4749cc6
Aman Taak <amantaak99@hotmail.com>
2024-08-08 01:32:06 +0100

    update

--- src/api/server.js
+++ src/api/server.js
@@ -503,9 +503,9 @@ async function handlePaymentIntentSucceeded(paymentIntent) {
 }
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static(path.join(__dirname, "../../build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../../build", "index.html"));
   });
 }
 
